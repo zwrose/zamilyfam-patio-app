6deadc71a8d4de10d3fd5105c65ca8e1d2798be8
Zach Rose <zach_rose@bose.com>
2019-11-24 00:10:46 -0500

    auth on status

--- routes/ifttt_status.js
+++ routes/ifttt_status.js
@@ -2,7 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.sendStatus(200);
+  var serviceKey = req.get('IFTTT-Service-Key');
+  var channelKey = req.get('IFTTT-Channel-Key');
+  var realKey = 'VPZA9GURJmf1SQ5joYgzQMZskTGHzzsBfjW3KUbg03roPhqWoOXIAQStTb8Zswc-';
+
+  if (serviceKey == realKey || channelKey == realKey) {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(401);
+  }
+  
 
 });
 
