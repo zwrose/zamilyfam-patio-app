557a857ca12667294d7f2df58151bd99c9e61799
Zach Rose <zach_rose@bose.com>
2019-11-24 00:25:15 -0500

    refactor auth middleware

--- routes/ifttt.js
+++ routes/ifttt.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/status', function(req, res, next) {
+var authCheck = function(req, res, next) {
   var serviceKey = req.get('IFTTT-Service-Key');
   var channelKey = req.get('IFTTT-Channel-Key');
   var realKey = 'VPZA9GURJmf1SQ5joYgzQMZskTGHzzsBfjW3KUbg03roPhqWoOXIAQStTb8Zswc-';
 
   if (serviceKey == realKey || channelKey == realKey) {
-    res.sendStatus(200);
+    next();
   } else {
     res.sendStatus(401);
   }
-  
+}
+
+router.use(authCheck);
+
+router.get('/status', function(req, res, next) {
+  res.sendStatus(200);  
 });
 
 router.post('/test/setup', function(req, res, next) {
