fc8d1a54c9b16ffe37d2ec057c3f2fd4b3f7a453
Zach Rose <zach_rose@bose.com>
2019-11-23 21:14:48 -0500

    basic forms processing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Zamily Patio Control',
+    result: false
+  });
+});
+
+router.post('/', function(req, res, next) {
+  var resultOutput = {
+    success: false,
+    description: "not set"
+  };
+  console.log(req.body.onOff);
+
+
+
+  res.render('index', {
+     title: 'Zamily Patio Control',
+     result: resultOutput
+  });
 });
 
 module.exports = router;
